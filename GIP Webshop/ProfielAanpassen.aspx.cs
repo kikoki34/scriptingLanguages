5f3a5268e956adbaed5813bf698717376722afb6
kikoki34 <kikoki@tutamail.com>
2024-07-16 18:10:45 +0300

    add new styles

---
+++ GIP Webshop/ProfielAanpassen.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace GIP_Webshop
+{
+    public partial class ProfielAanpassen : System.Web.UI.Page
+    {
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAanpassen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTerug_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("EigenProfiel.aspx");
+        }
+    }
+}
