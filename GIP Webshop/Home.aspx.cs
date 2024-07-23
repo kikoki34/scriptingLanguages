a9e4a1a7586db2bfa0341ce7d2cd676398497a74
kikoki34 <kikoki@tutamail.com>
2024-07-23 18:10:45 +0300

    Optimize performance

---
+++ GIP Webshop/Home.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GIP_Webshop
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAanmelden_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Login.aspx");
+        }
+
+        protected void btnRegistreren_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Registreren.aspx");
+        }
+
+        protected void btnBeheerder_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("LoginBeheerder.aspx");
+        }
+
+        protected void btnProducten_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Producten.aspx");
+        }
+    }
+}
