325c73d0093d4ba3c2245ae705c4a3fdd7eace41
kikoki34 <kikoki@tutamail.com>
2024-07-24 18:10:45 +0300

    Improve performance of data export

---
+++ GIP Webshop/EigenProfiel.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GIP_Webshop
+{
+    public partial class EigenProfiel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAanpassen_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("ProfielAanpassen.aspx");
+        }
+
+        protected void btnProducten_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Producten.aspx");
+        }
+
+        protected void btnEigenBes_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("MijnBestellingen.aspx");
+        }
+
+        protected void btnAfmelden_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Home.aspx");
+        }
+    }
+}
