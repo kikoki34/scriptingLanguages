b2bbc28fcb7792543646c2bb9e129c36024a66ca
kikoki34 <kikoki@tutamail.com>
2024-07-27 18:10:45 +0300

    Fix test cases

---
+++ GIP Webshop/BeheerProducten.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GIP_Webshop
+{
+    public partial class BeheerProducten : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTerug_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Beheerder.aspx");
+        }
+    }
+}
