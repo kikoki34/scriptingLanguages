5a32045cb1ad37f343c3db17326ea4feb0c247d1
kikoki34 <kikoki@tutamail.com>
2024-07-30 18:10:45 +0300

    improve database queries

---
+++ GIP Webshop/BeheerKlanten.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GIP_Webshop
+{
+    public partial class BeheerKlanten : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTerug_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Beheerder.aspx");
+
+        }
+    }
+}
