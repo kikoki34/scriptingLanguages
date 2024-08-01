8e309d1eba315c7b508b62bed5986cae2fb60546
kikoki34 <kikoki@tutamail.com>
2024-08-01 18:10:45 +0300

    improve performance

---
+++ GIP Webshop/Beheerder.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GIP_Webshop
+{
+    public partial class Beheerder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnKlanten_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("BeheerKlanten.aspx");
+        }
+
+        protected void btnProducten_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("BeheerProducten.aspx");
+        }
+
+        protected void btnBestellingen_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("BeheerBestellingen.aspx");
+        }
+
+        protected void btnAfmelden_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Home.aspx");
+        }
+    }
+}
