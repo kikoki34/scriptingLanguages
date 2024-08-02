5c7d17599e8e5a63faf52a113a6eafe3259be127
kikoki34 <kikoki@tutamail.com>
2024-08-02 18:10:45 +0300

    Fix layout issue

---
+++ GIP Webshop/BeheerBestellingen.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GIP_Webshop
+{
+    public partial class BeheerBestellingen : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dgvExtra.Visible = false;
+        }
+
+        protected void btnTerug_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Beheerder.aspx");
+        }
+
+        protected void dgvbestellingen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dtsExtra.FilterExpression = "BestellingsID =" + dgvbestellingen.SelectedRow.Cells[1].Text;
+            dgvExtra.DataBind();
+            dgvExtra.Visible = true;
+        }
+
+        protected void DetailsView1_PageIndexChanging(object sender, DetailsViewPageEventArgs e)
+        {
+
+        }
+    }
+}
